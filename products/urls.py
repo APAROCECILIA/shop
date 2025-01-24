8fe5afdecdd60916ca8768e5d0da39e44f35e0f5
APARO CECILIA <ceciliaheavens256@gmail.com>
2025-01-24 18:01:12 +0300

    first commit

---
+++ products/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),          # Home page
+    path('error/', views.error, name='404'),     
+    path('cart/', views.cart, name='cart'), 
+    path('chackout/', views.chackout, name='chackout'), 
+    path('contact/', views.contact, name='contact'), 
+    path('shopdetail/', views.shopdetail, name='shopdetail'), 
+    path('shop/', views.shop, name='shop'), 
+    path('testimonial/', views.testimonial, name='testimonial'), 
+]
+
---
+++ products/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+def error(request):
+    return render(request, 'products/404.html')
+
+def cart(request):
+    return render(request, 'products/cart.html')
+
+def chackout(request):
+    return render(request, 'products/chackout.html')
+
+
+def contact(request):
+    return render(request, 'products/contact.html')
+
+def index(request):
+    return render(request, 'products/index.html')
+
+def shopdetail(request):
+    return render(request, 'products/shop-detail.html')
+
+def shop(request):
+    return render(request, 'products/shop.html')
+
+def testimonial(request):
+    return render(request, 'products/testimonial.html')
